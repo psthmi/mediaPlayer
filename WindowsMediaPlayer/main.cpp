70b33d269f3230be71b48d93118389c36b89c673
Rama <psthmiproject@gmail.com>
2017-09-30 16:10:57 -0400

    rama mediaPlayer

---
+++ WindowsMediaPlayer/main.cpp
@@ -0,0 +1,15 @@
+#include <QGuiApplication>
+#include <QQmlApplicationEngine>
+#include <QIcon>
+
+int main(int argc, char *argv[])
+{
+    QGuiApplication app(argc, argv);
+    app.setWindowIcon(QIcon("qrc:/icon1.ico"));
+    QQmlApplicationEngine engine;
+    engine.load(QUrl(QStringLiteral("qrc:/main.qml")));
+    if (engine.rootObjects().isEmpty())
+        return -1;
+
+    return app.exec();
+}
